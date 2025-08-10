bdc2f20a9ad5b0a4c8199be3779b5cb36c963e4b
Yonatan Tizazu <yonatantizazu12@gmail.com>
2025-08-10 13:44:56 +0300

    feat: interface and struct

--- recursion/main.go
+++ recursion/main.go
@@ -4,10 +4,27 @@ import (
 	"fmt"
 )
 
+type Rectangle struct {
+	length float64
+	width  float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.length * r.width
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return r.length*2 + r.width*2
+}
+
 func main() {
 
-	name := "Yonatan Tizazu"
-	age := 24
+	name := Rectangle{
+		length: 4,
+		width:  2,
+	}
 
-	fmt.Printf("My name is %c and i am %d years old", name[0], age)
+	fmt.Println(name.Perimeter())
+	fmt.Println(name.Area())
+
 }
